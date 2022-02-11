64f438c05ae0d98c0ee3e89dd27c87dad6b3b3d7
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 12:29:47 +0500

    Initial Commit

---
+++ Yan_SwiftUI_CustomTabBar/Yan_SwiftUI_CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_CustomTabBarApp.swift
+//  Yan_SwiftUI_CustomTabBar
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
